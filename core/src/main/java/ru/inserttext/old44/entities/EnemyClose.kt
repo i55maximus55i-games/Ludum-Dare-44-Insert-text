f917693318eb3d914819e818bb1fe340c3ddea64
55_maximus_55 <lukinosh@gmail.com>
2019-04-28 01:37:04 +0600

    good

--- core/src/main/java/ru/inserttext/old44/entities/EnemyClose.kt
+++ core/src/main/java/ru/inserttext/old44/entities/EnemyClose.kt
@@ -1,6 +1,7 @@
 package ru.inserttext.old44.entities
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.math.MathUtils
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
@@ -8,21 +9,45 @@ import ru.inserttext.old44.Main
 
 class EnemyClose(val world: World, position: Vector2, scale: Float) : Enemy(world, position, scale) {
 
-    private val texture = Main.assets.getTexture("enemyclose.png")
+    //TODO сделать здесь анимацию
+
+    private val texture = Main.assets.getTexture("enemy2.png")
+    val textureRegion = TextureRegion(texture)
+    val animDuration = 0.3f
+    var animTimer = 0f
+    val attackAnimDuration = 0.2f
+    val attackAnimTimer = 0f
 
     val distanceMin = 1.2f
     val distanceMax = 2f
     val speedMin = 4f
     val speedMax = 5f
     val attackDst = 1.5f
-    val attackAnimDuration = 0.2f
     val gobackDuration = 0.4f
 
     var goBackTimer = 0f
 
+    var hp = 2
+    var hitEffect = 0f
+
     override fun draw(batch: SpriteBatch) {
         if (texture != null) {
-            batch.draw(texture, body.position.x / scale - texture.width / 2, body.position.y / scale - texture.height / 2)
+            textureRegion.texture = texture
+            textureRegion.setRegion(0, 0, 64, 64)
+
+            if (hp <= 0) {
+                textureRegion.setRegion(0, 128, 64, 64)
+                batch.draw(textureRegion, body.position.x / scale - textureRegion.regionWidth / 2, body.position.y / scale - textureRegion.regionHeight / 2)
+            } else if (hitEffect < 0 || hitEffect % 0.1f > 0.05f) {
+                if (attackAnimTimer <= attackAnimDuration) {
+                    val t = (animTimer / (animDuration / 4)).toInt() + 2
+                    textureRegion.setRegion(64 * t, 64 * (2 - hp), 64, 64)
+                } else {
+                    val t = (animTimer / (animDuration / 2)).toInt()
+                    textureRegion.setRegion(64 * t, 64 * (2 - hp), 64, 64)
+                }
+                batch.draw(textureRegion, body.position.x / scale - textureRegion.regionWidth / 2, body.position.y / scale - textureRegion.regionHeight / 2)
+            }
         }
     }
 
